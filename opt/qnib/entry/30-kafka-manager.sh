750212111e85d0cf7e0a0545bcc78c5d6f71d173
Christian Kniep <christian@qnib.org>
2017-09-18 09:20:08 +0200

    bugfix ZK_SERVER to ZOOKEEPER_HOSTS

--- opt/qnib/entry/30-kafka-manager.sh
+++ opt/qnib/entry/30-kafka-manager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat /opt/qnib/kafka/manager/conf/application.conf |sed -e "s/ZK_SERVER/${ZK_SERVER}/" \
+cat /opt/qnib/kafka/manager/conf/application.conf |sed -e "s/ZOOKEEPER_HOSTS/${ZOOKEEPER_HOSTS}/" \
   > /opt/kafka-manager/conf/application.conf
